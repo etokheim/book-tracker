6f0c138df6e1b898e4a277f97274ec816853654a
Erling Tokheim <e@tokheim.no>
2020-06-17 13:23:34 +0200

    Refactor: Headers into a new Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 // import * as BooksAPI from './BooksAPI'
-import './App.css'
+import './App.css';
+import Header from './components/Header';
 
 class BooksApp extends React.Component {
 	state = {
@@ -10,37 +11,19 @@ class BooksApp extends React.Component {
 		 * users can use the browser's back and forward buttons to navigate between
 		 * pages, as well as provide a good URL they can bookmark and share.
 		 */
-		showSearchPage: false
+		showSearchPage: false,
 	}
 
 	render() {
 		return (
 			<div className="app">
+				<Header showSearch={false} />
 				{this.state.showSearchPage ? (
-					<div className="search-books">
-						<div className="search-books-bar">
-							<button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-							<div className="search-books-input-wrapper">
-								{/*
-									NOTES: The search from BooksAPI is limited to a particular set of search terms.
-									You can find these search terms here:
-									https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-									However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-									you don't find a specific author or title. Every search is limited by search terms.
-								*/}
-								<input type="text" placeholder="Search by title or author"/>
-
-							</div>
-						</div>
-						<div className="search-books-results">
+					<div className="search-books-results">
 						<ol className="books-grid" />
 					</div>
 				) : (
 					<div className="list-books">
-						<div className="list-books-title">
-							<h1>MyReads</h1>
-						</div>
 						<div className="list-books-content">
 							<div>
 								<div className="bookshelf">
