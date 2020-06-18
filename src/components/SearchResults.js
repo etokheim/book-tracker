fd01d7b91f223ad1160941dc4918149229856077
Erling Tokheim <e@tokheim.no>
2020-06-18 17:34:53 +0200

    Feat: Search for books

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import * as booksApi from "../BooksAPI";
+import Book from "./Book";
 
 export default class SearchResults extends Component {
+	state = {
+		searchResults: []
+	}
+
 	static propTypes = {
 		toggleSearch: PropTypes.func.isRequired
 	}
@@ -15,16 +21,34 @@ export default class SearchResults extends Component {
 		this.props.toggleSearch(false);
 	}
 
-	handleSearchInput = (input) => {
-		console.log(input);
+	handleSearchInput = async (query) => {
+		const searchResults = await booksApi.search(query);
+
+		// The API returns nothing when it receives an empty query...
+		if(!searchResults) {
+			console.error("Something went wrong with the API. It returned nothing.");
+			return;
+		}
+
+		// Handle server errors
+		// Note: the server responds with an "Empty query" error when it gets no hits.
+		if(searchResults.error) {
+			console.error(`Server responded with error: ${searchResults.error}`);
+			searchResults = [];
+		}
+
+		this.setState({
+			searchResults
+		})
 	}
 
 	render() {
-		const { searchQuery } = this.props;
+		const { searchResults } = this.state;
 		return (
 			<div className="search-books-results">
-				<span>{ searchQuery }</span>
-				<ol className="books-grid" />
+				{searchResults.map( book => (
+					<Book handleMoveBook={ () => {} } book={ book } key={ book.id } />
+				))}
 			</div>
 		)
 	}
